b207f5cb07d4372cdf945c0e1472341047bb5bc6
alexhulbert <alex@alexhulbert.com>
2022-08-29 21:40:40 -0400

    Add icon source notice

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,8 @@ import './Footer.css'
 export default ({ globalSettings, socialSettings, navLinks }) => (
   <footer className='Footer'>
     <div className='container taCenter'>
-      <span>© 2022 Nikki Sigurdson</span>
+      <span>© 2022 Nikki Sigurdson</span><br />
+      <a className="legal" href="https://www.flaticon.com/free-icons/cherry" title="cherry icons">Cherry icon by Freepik from Flaticon</a>
     </div>
   </footer>
 )
