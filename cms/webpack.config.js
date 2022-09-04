1c39b4458844e40c4fb69e497da9ea8a80bbff16
alexhulbert <alex@alexhulbert.com>
2022-09-04 00:56:28 -0400

    Change babel root mode and delete redundant files

--- cms/webpack.config.js
+++ cms/webpack.config.js
@@ -27,6 +27,7 @@ module.exports = {
             '@babel/plugin-proposal-object-rest-spread',
             '@babel/plugin-proposal-class-properties',
           ],
+          rootMode: 'upward'
         },
       },
       {
